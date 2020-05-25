a50d0093b7167bf69e675fb420c9e0f4eb2bb7e9
unknown <langeraertarnaud@gmail.com>
2020-05-25 14:46:38 +0200

    Made a script to illustrate how to use the IR system.

---
+++ use_example.py
@@ -0,0 +1,10 @@
+# First import the decision logic extractor
+from decision_logic_extractor import *
+# ---- Step 1: Make a spacy document of the sentence
+doc = sp("If the day is rainy, sell waffles.")
+
+# ---- Step 2a: Generate the high level logic
+condition_consequence_extractor_v3(doc)
+
+# ---- Step 2b: Get the low level decision logic
+get_full_dmn_rule(doc)
